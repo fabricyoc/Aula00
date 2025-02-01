46665557e1e65e0193d18b0d5087041695f82dc6
Fabricyo <fazin_c@hotmail.com>
2025-02-01 20:50:14 +0000

    Alerta em JS

--- Aula01/Pratica01.js
+++ Aula01/Pratica01.js
@@ -1,5 +1,6 @@
-let first_name = "roberto";
-let last_name = "carlos";
-let full_name = first_name + " " + last_name;
-
-console.log("Seu nome é: " + full_name);
+function exibirNome(){
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let full_name = fname + " " + lname;
+    alert(full_name.toUpperCase());
+}
